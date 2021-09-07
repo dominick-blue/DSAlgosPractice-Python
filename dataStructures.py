f988176854947556294a36b86649e28951c08fb9
Dominick Blue <dominickblue@meshgrid.us>
2021-09-07 12:02:30 -1000

    Initial commit - capturing my data structures practice.

---
+++ dataStructures.py
@@ -0,0 +1,72 @@
+# The purpose of this file is to document practice of data structures within the Python 3 language
+
+# Pythons Built-In Data structures
+
+# Sequence Types: We can access elements within these types with indexing
+
+# Strings
+
+# Indexing
+x = 'frog'
+# print(x[0]) # Gives us 'f'
+
+# Slicing
+x = 'Computer'
+comp = x[:4].lower()
+cpe = x[::3].lower()
+# print(x[1:4]) # Starts at the 'o'. Start is inclusive and End is Non-inclusive
+# print(x[1:6:2]) # Retrieves every SECOND step or element
+# print(x[3:])
+# print(x[:5])
+# print(x[-1])
+# print(x[-3:])
+# print(x[:-2])
+# print(cpe)
+
+
+# print('x' in x)
+
+# lists
+
+# Adding and Concatenating
+
+y = ['cow', 'chicken', 'horse', 'car']
+# print('cow' not in y) # A way of checking membership
+
+# Iterating through the items in a sequence
+
+# for index, item in enumerate(y):
+#     print(index, item)
+
+xy = [y[1]] + ['house', 'airplane']
+
+num_list = [9, 10, 11, 2, 3]
+num_list
+print(num_list)
+
+# print(xy)
+# tuples # Biggest difference between tuples and lists are that tuples are immutable and lists are mutable
+# tuples are faster than lists
+
+z = ('Dominick', 'Jasmine', 'Beverly', 'Diana')
+# print(min(z)) # For strings will print the alpha-numerically smallest element ('Beverly' is the first element)
+
+
+
+zy = z + ('JaeLynn',)
+
+zy = zy * 3
+# print(zy)
+# sets
+
+
+
+# General
+# queues
+# stacks
+# heaps
+# linked lists
+
+# Algos
+
+# binary search trees
